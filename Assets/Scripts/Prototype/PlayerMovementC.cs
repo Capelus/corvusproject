a26b49c6f8afc03cd5c2511b90937cdf14756c08
Extermask23 <carles.capel@gmail.com>
2020-08-13 10:10:55 +0200

    Implemented spaceship movement following Bezier Path

--- Assets/Scripts/Prototype/PlayerMovementC.cs
+++ Assets/Scripts/Prototype/PlayerMovementC.cs
@@ -1,35 +1,169 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using PathCreation;
 
 public class PlayerMovementC : MonoBehaviour
 {
+    //OPTIONS
+    public bool allowHorizontalMovement;
+    public bool invertVerticalAxis;
 
-    public float xspeed = 0.8f;
-    public float yspeed = 0.1f;
+    public PathCreator pathCreator;
+    public EndOfPathInstruction endOfPathInstruction;
+    float distanceTravelled;
 
-    private Rigidbody rb;
+    //MOVEMENT PARAMETERS
+    [System.Serializable]
+    public class Movement
+    {
+        public float maxSpeed = 60;
+        public float currentSpeed = 0;
+        public float acceleration = 20;
+        public float handlingSpeed = 15;
+    }
+    public Movement movement;
 
-    // Start is called before the first frame update
-    void Start(){
+    //SHOT REFERENCES AND PARAMETERS
+    [System.Serializable]
+    public class Blaster
+    {
+        public Transform shotSpawn1, shotSpawn2;
+        public GameObject shot;
+        public float cadence = 0.2f;
+    }
+    public Blaster blaster;
 
-        rb = GetComponent<Rigidbody>();
+    float lCadence;
+    bool lastShotSwitch = false;
+
+    //LIMITS
+    [System.Serializable]
+    public class ScreenLimits
+    {
+        public float bottomLimit = 1, topLimit = 5.5f, leftLimit = 11f, rightLimit = 19f;
+    }
+    public ScreenLimits screenLimits;
+
+    //OTHER
+    Quaternion baseRotation;
+
+    private void Start()
+    {
+        lCadence = blaster.cadence;
+        baseRotation = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * yspeed;
-        //Clamper
-        if(transform.position.y < 1.5f) transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
-        if (transform.position.y > 10) transform.position = new Vector3(transform.position.x, 10.0f, transform.position.z);
-        //
 
-        if (transform.position.y >= 1.5f && transform.position.y <= 10.0f){
-            transform.Translate(0, Mathf.Clamp(translation,-0.1f,0.1f), 0);
+     
+
+        float vMove = Input.GetAxis("Vertical");
+        if (invertVerticalAxis)
+            vMove = -vMove;
+
+        float hMove = 0;
+        if (allowHorizontalMovement)
+            hMove = Input.GetAxis("Horizontal");
+
+        //CHECK IZQUIERDA
+        if (hMove < 0)
+        {
+            if (transform.position.z <= screenLimits.leftLimit)
+                hMove = 0;
+
+            else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation * Quaternion.Euler(0, 0, 15), 0.05f);
         }
 
+        //CHECK DERECHA
+        else if (hMove > 0)
+        {
+            if (transform.position.z >= screenLimits.rightLimit)
+                hMove = 0;
 
-        transform.Translate(0, 0, xspeed);
+            else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation * Quaternion.Euler(0, 0, -15), 0.05f);
+        }
+
+        else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation, 0.02f);
+
+        //CHECK ARRIBA
+        if (vMove > 0)
+        {
+            if (transform.position.y >= screenLimits.topLimit)
+                vMove = 0;
+
+            else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation * Quaternion.Euler(-15, 0, 0), 0.05f);
+        }
+
+        //CHECK ABAJO
+        else if (vMove < 0)
+        {
+            if (transform.position.y <= screenLimits.bottomLimit)
+                vMove = 0;
+
+            else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation * Quaternion.Euler(15, 0, 0), 0.05f);
+        }
+
+        else transform.rotation = Quaternion.Slerp(transform.rotation, baseRotation, 0.02f);
+
+        //ACCELERATE
+        if (Input.GetButton("Accelerate"))
+        {
+            while (Mathf.Abs(movement.currentSpeed - movement.maxSpeed) > Mathf.Epsilon)
+            {
+                if (movement.currentSpeed < movement.maxSpeed) movement.currentSpeed += movement.acceleration * Time.deltaTime;
+                else movement.currentSpeed -= movement.acceleration * Time.deltaTime;
+                break;
+            }
+        }
+
+        else movement.currentSpeed = Mathf.Lerp(movement.currentSpeed, 0, 0.05f);
+
+        //BARREL ROLL
+        if (Input.GetButton("Roll"))
+            GetComponent<Animation>().Play();
+
+
+        if (pathCreator != null)
+        {
+            distanceTravelled += movement.currentSpeed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+        }
+
+        //SHOOT
+        if (Input.GetButton("Fire"))
+        {
+            lCadence -= Time.deltaTime;
+            if (lCadence < 0)
+            {
+                GameObject s;
+
+                if (lastShotSwitch)
+                    s = Instantiate(blaster.shot, blaster.shotSpawn2.position, blaster.shotSpawn2.rotation);
+
+                else
+                    s = Instantiate(blaster.shot, blaster.shotSpawn1.position, blaster.shotSpawn1.rotation);
+
+                s.GetComponent<ShotBehaviour>().speed += movement.currentSpeed;
+                lCadence = blaster.cadence;
+                lastShotSwitch = !lastShotSwitch;
+            }
+        }
+
+        //APPLY MOVEMENT
+        Vector3 movementDirection = Vector3.up * vMove + Vector3.forward * hMove + Vector3.left;
+        //Move(movementDirection, movement.maxSpeed);
+    }
+
+    void Move(Vector3 dir, float speed)
+    {
+        // transform.position += new Vector3(dir.x * movement.currentSpeed, dir.y * movement.handlingSpeed, dir.z * movement.handlingSpeed) * Time.deltaTime;
+
+        distanceTravelled += movement.currentSpeed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
     }
 }
