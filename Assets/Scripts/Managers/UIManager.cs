b50504f8c94c5abc977dac954b135165841f4d86
Extermask23 <carles.capel@gmail.com>
2020-09-01 16:19:31 +0200

    TimeChart Base and Lap Checkpoint

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -17,7 +17,7 @@ public class UIManager : MonoBehaviour
     [System.Serializable]
     public class UIWarmUp
     {
-        //LIST OF UI ELEMENTS
+        //LIST OF UIW ELEMENTS
         public Text countDown;
 
     }
@@ -32,6 +32,7 @@ public class UIManager : MonoBehaviour
         public Slider energyBarMid;
         public Slider energyBarHigh;
         public Text raceTimer;
+        public Text[] timeChart;
     }
     public UIElements UI;
 
@@ -39,7 +40,7 @@ public class UIManager : MonoBehaviour
     {
         player = GameManager.Instance.player;
 
-
+        UI.timeChart = new Text[8];
         UI.energyBarLow.minValue = 0;
         UI.energyBarMid.minValue = (player.energyParameters.maxEnergy / 3);
         UI.energyBarHigh.minValue = (player.energyParameters.maxEnergy / 3) * 2;
@@ -57,18 +58,21 @@ public class UIManager : MonoBehaviour
         UI.energyBarMid.value = player.l_energy;
         UI.energyBarHigh.value = player.l_energy;
         UIW.countDown.text = GameManager.Instance.raceManager.countDown.ToString("f0");
+      
+
+        //COUNTDOWN--------------------------------------------
         if (GameManager.Instance.raceManager.countDown <= 1.0f)
         {
             player.GetComponent<PlayerInput>().inputEnabled = true;
             UIW.countDown.text = "GO!";
 
         }
-        if (GameManager.Instance.raceManager.countDown == 0.0f)
+        if (GameManager.Instance.raceManager.countDown <= 0.0f)
         {
             UIW.countDown.text = "";
         }
 
-        //UI.raceTimer.text = GameManager.Instance.raceManager.minutes.ToString("f0") + ":" + GameManager.Instance.raceManager.seconds.ToString("f0") + ":" + GameManager.Instance.raceManager.milliseconds.ToString();
+        //RACE TIMER--------------------------------------------------------
         UI.raceTimer.text = GameManager.Instance.raceManager.convertedTime;
 
     }
